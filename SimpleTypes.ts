fc50b3ceb92f2173f211bda82fe41b628880cb5a
Narvienn <40996312+Narvienn@users.noreply.github.com>
2024-09-30 18:26:18 +0200

    Create SimpleTypes.ts

---
+++ SimpleTypes.ts
@@ -0,0 +1,10 @@
+/* Simple types in TS are: 
+- boolean
+- number (integer AND float!)
+- string (text, "") */
+
+//EXPLICIT TYPING - define variable type when declaring it:
+let myFirstName: string = "Zos";
+
+//IMPLICIT TYPING - variable type is assumed from the first value declared for the variable:
+let myLastName - "Vonho";
